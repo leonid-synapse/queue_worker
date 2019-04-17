2c79ff65fd1be85ccab3789e6541c678709175cd
Leonid Rasumov <masdzen@gmail.com>
2019-04-17 14:42:30 +0300

    process connect exception

--- src/Services/RabbitmqService.php
+++ src/Services/RabbitmqService.php
@@ -4,6 +4,7 @@ namespace Drupal\queue_worker\Services;
 
 use Drupal\rabbitmq\Connection;
 use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Exception\AMQPProtocolConnectionException;
 
 /**
  * Class RabbitmqService.
@@ -21,35 +22,65 @@ class RabbitmqService implements RabbitmqServiceInterface {
    * Constructs a new RabbitmqService object.
    */
   public function __construct(Connection $rabbitConnection) {
-    $this->connection = $rabbitConnection->getConnection();
+    try {
+      $this->connection = $rabbitConnection->getConnection();
+    }
+    catch (AMQPProtocolConnectionException $e) {
+      \Drupal::logger(__CLASS__ . __LINE__)->notice(
+        '@message', ['@message' => $e->getMessage()]
+      );
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function pushMessage(string $queueName, array $data) {
-    $channel = $this->connection->channel();
-    $routing_key = $queue_name = $queueName;
-    $channel->queue_declare($queue_name, FALSE, TRUE, FALSE, TRUE);
-    $message = new AMQPMessage(json_encode($data));
-    $channel->basic_publish($message, '', $routing_key);
-    $channel->close();
-    $this->connection->close();
+    if (empty($this->connection)) {
+      \Drupal::logger(__CLASS__ . __LINE__)->notice(
+        '@message', ['@message' => 'Empty connection']
+      );
+      return;
+    }
+    try {
+      $channel = $this->connection->channel();
+      $routing_key = $queue_name = $queueName;
+      $channel->queue_declare($queue_name, FALSE, TRUE, FALSE, TRUE);
+      $message = new AMQPMessage(json_encode($data));
+      $channel->basic_publish($message, '', $routing_key);
+      $channel->close();
+      $this->connection->close();
+    }
+    catch (Exception $e) {
+      \Drupal::logger(__CLASS__ . __LINE__)->notice(
+        '@message', ['@message' => $e->getMessage()]
+      );
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function pushMessages(string $queueName, array $dataArray) {
-    $channel = $this->connection->channel();
-    $routing_key = $queue_name = $queueName;
-    $channel->queue_declare($queue_name, FALSE, TRUE, FALSE, TRUE);
-    foreach ($dataArray as $data) {
-      $message = new AMQPMessage(json_encode($data));
-      $channel->basic_publish($message, '', $routing_key);
+    if (empty($this->connection)) {
+      return;
     }
-    $channel->close();
-    $this->connection->close();
+    try {
+      $channel = $this->connection->channel();
+      $routing_key = $queue_name = $queueName;
+      $channel->queue_declare($queue_name, FALSE, TRUE, FALSE, TRUE);
+      foreach ($dataArray as $data) {
+        $message = new AMQPMessage(json_encode($data));
+        $channel->basic_publish($message, '', $routing_key);
+      }
+      $channel->close();
+      $this->connection->close();
+    }
+    catch (Exception $e) {
+      \Drupal::logger(__CLASS__ . __LINE__)->notice(
+        '@message', ['@message' => $e->getMessage()]
+      );
+    }
   }
 
 }
